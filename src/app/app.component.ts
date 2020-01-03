4380c34e1a77a4b98166d981b994cd3c3a792d59
Alex Chang <achang209@gmail.com>
2020-01-03 06:12:31 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'auto-service-tracker-client';
+  title = 'Auto Service Tracker';
 }
