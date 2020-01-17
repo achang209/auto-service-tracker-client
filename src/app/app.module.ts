7ee67056405d4e7f1797fc08825bedd69189f317
Alex Chang <achang209@gmail.com>
2020-01-17 10:44:27 -0600

    create invoice search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { InvoiceListComponent } from './invoice-list/invoice-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormAddInvoiceComponent } from './form-add-invoice/form-add-invoice.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { InvoiceSearchComponent } from './invoice-search/invoice-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InvoiceListComponent,
-    FormAddInvoiceComponent
+    FormAddInvoiceComponent,
+    InvoiceSearchComponent
   ],
   imports: [
     BrowserModule,
