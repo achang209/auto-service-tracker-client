8a99f026ab4c4a15a2d95fe7e63764953facc7d5
Alex Chang <achang209@gmail.com>
2020-01-24 13:13:46 -0600

    refactor invoice form and dependencies to consume ws changes

---
+++ src/app/repair.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Repair } from './repair';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RepairService {
+  private repairUrl: string = "//localhost:8080/repair"
+
+  constructor(private http: HttpClient) { }
+
+  getRepairs(): Observable<Repair[]> {
+    return this.http.get<Repair[]>(this.repairUrl);
+  }
+}
